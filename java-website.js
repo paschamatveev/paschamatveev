a650c6c4422abdb2eff3f215e5af820dea9a0b35
Nadia Matveeva <nadiamat@uw.edu>
2024-12-20 10:31:55 -0800

    buttons (broken)

---
+++ java-website.js
@@ -0,0 +1,8 @@
+function sectionDropdown(){
+    var x = document.getElementById("section-dropdown");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
